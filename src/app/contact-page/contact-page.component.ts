4c8bd128c5642c63c9ea017bd4ab881df18bc14a
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-12-11 23:55:35 +0400

    Update contact form

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -7,19 +7,6 @@ import { Component, HostListener } from '@angular/core';
 })
 export class ContactPageComponent {
 
-  decoration_variable : boolean = false;
-
-  @HostListener("document:scroll")
-  scrollFunction(){
-    let pos = document.documentElement.scrollTop;
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-
-    if(scrollValue >= 98) {
-      this.decoration_variable = true;
-    }else {
-      this.decoration_variable = false;
-    }
-  }
+  
 
 }
